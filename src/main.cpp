09f8d92ff26734824d19566d3fde2913dbaebdd1
mishthefish <pablomishi123@gmail.com>
2021-09-17 15:57:43 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,7 @@ void autonomous(void) {
 void usercontrol(void) {
   // User control code here, inside the loop
   while (1) {
-    while(Controller1.ButtonA.pressing()){
+    if(Controller1.ButtonA.pressing()){
       Motor3.spin(forward);
     }
     // This is the main execution loop for the user control program.
