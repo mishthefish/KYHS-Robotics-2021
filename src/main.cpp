3a90cab0095419e13978668bd052dafe7d025476
mishthefish <pablomishi123@gmail.com>
2021-09-17 16:00:27 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -73,6 +73,7 @@ void usercontrol(void) {
     if(Controller1.ButtonA.pressing()){
       Motor3.spin(forward);
     }
+    
     // This is the main execution loop for the user control program.
     // Each time through the loop your program should update motor + servo
     // values based on feedback from the joysticks.
